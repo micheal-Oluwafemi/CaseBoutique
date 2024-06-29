fb20a6b57ef2861cfdeb8ef7959a8bad593b41b6
micheal-Oluwafemi <raytech401@gmail.com>
2024-06-29 12:24:01 +0100

    Creating steps navigation

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { ReactNode } from "react";
+import Steps from "@/components/Steps"
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className='flex flex-1 flex-col'>
+    <Steps/>
       {children}
     </MaxWidthWrapper>
   );
